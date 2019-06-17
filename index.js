ad2d6c1f9e88e1511dd847e39713664b48015660
hitfangsong <sof10@pitt.edu>
2019-06-17 10:02:53 -0400

    First Modification

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 //one kind of import syntax
 //common js module at server side
 const express = require("express");
+//import passport js
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//tell passport to use googleStategy
+passport.use(new GoogleStrategy());
 //generate a new application
 const app = express();
 //router
-//create a brand new handler
+//create a brand new router handler
 app.get('/',(req,res)=>{
   //error function
-  res.send({'hi':'there'});
+  res.send({'hi':'SongFang'});
 });
+
 //express telling node to listen on port 5000
 const PORT = process.env.PORT||5000
 app.listen(PORT);
