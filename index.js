8ec062fe69a6be9dae30a6334333825772ac94c3
Song-Fang <sof10@pitt.edu>
2020-02-11 02:15:05 -0500

    fix bugs

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ if(process.env.NODE_ENV==='prodution'){
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('*',(req,res)=>{
+  app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   })
 }
